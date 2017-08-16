bd87c1a2b5885e3bd4553ba9bb34c1c163edd9f3
bouracha <bouracha@tcd.ie>
2017-08-16 21:03:26 +0100

    Removed loops for featues and made a feature create file that loops through all feature functions

---
+++ data_preparation/feature_create.m
@@ -0,0 +1,19 @@
+function [matrix_features] = feature_create(min_indice, max_indice, nums, data);
+
+
+matrix_features = [];
+for i = min_indice:max_indice
+    features = [i, data(i)];
+    for n = nums
+        data_quanta = quantum_standardisation(i, n, data);
+        
+        [s1, s2] = trendline(i, n, data_quanta);
+        K = oscillator(i, n, data_quanta);
+        firstdiff = diff1(i, n, data_quanta);
+        secondDiff = diff2(i, n, data_quanta);
+        features = [features, s1, s2, K, firstdiff, secondDiff];
+    end
+    matrix_features = [matrix_features; features];
+end     
+ 
+end
